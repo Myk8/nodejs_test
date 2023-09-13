89fc57e94c50842a4798293417d876e64f133b67
Mikey Mazza <mikeymazza0802@gmail.com>
2023-09-13 13:39:58 +0200

    change

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ console.log(uri)
 
 const client = new MongoClient(uri)
 const dbname = "Mybase"
+const collection_name="account"
 
+
+
+const accountCollection =client.db(dbname).collection(collection_name)
+
+
+// Connect to the database
 const connectToDatabase = async () => {
 try{
     await client.connect();
@@ -40,9 +47,50 @@ try{
 };  */
  
 
+
+/* 
+ insertOne method()
+
+const sampleAccount ={
+    account_holder: "Mazza Mikey",
+    account_id: "MBB829001337",
+    account_type: "cheking",
+    balace: 50352434,
+    last_updated: new Date(),
+}
+
+
+
+ let result= await accountCollection.insertOne(sampleAccount) 
+ console.log('Inserted document: '+result.insertedId)  
+*/
+
+
+
+
+//insertmany method()
+ sampleAccounts = [{
+  account_id:"MDB011234813",
+  account_holder: "Ada Lovelace",
+  account_type: "checking",
+  balace: 60218,
+},
+{
+    account_id:"MDB829000001",
+    account_holder: "Muhammad ibn Musa al-Khwarizmi",
+    account_type: "savings",
+    balace: 267914249,
+},
+]
+
+
 const main = async () =>{
 try{
     await connectToDatabase();
+
+ let result= await accountCollection.insertMany(sampleAccounts)            //replace                                
+ console.log('Inserted'+result.insertedCount+' documents')   //replace
+console.log(result)
 } catch (err){
     console.error('Error connecting to the database: '+ err);
 } finally{
