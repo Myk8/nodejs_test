32e597d0566c885760cd481fcf2579fe867a0916
Mikey Mazza <mikeymazza0802@gmail.com>
2023-09-13 13:41:17 +0200

    change_2

--- app.js
+++ app.js
@@ -24,49 +24,6 @@ try{
 };
 
 
-/*const account = {
-    _id : new ObjectId ("6312fa15cecacc1bc12a23"),
-    account_id: "MDB955769550",
-    account_holder: "Addison Shelton",
-    account_type: "checking",
-    balance: 1971.89,
-    transfers_complete:[
-        "TR644995264",
-        "TR962476523",
-        "TR137596327",
-        "TR654782494",
-        "TR467021469",
-        "TR910368742",
-    ],
-    address:{
-        city: "RIDGEWOOD",
-        zip: 11385,
-        street: "MENAHAN ST",
-        number : 1721,
-    },
-};  */
- 
-
-
-/* 
- insertOne method()
-
-const sampleAccount ={
-    account_holder: "Mazza Mikey",
-    account_id: "MBB829001337",
-    account_type: "cheking",
-    balace: 50352434,
-    last_updated: new Date(),
-}
-
-
-
- let result= await accountCollection.insertOne(sampleAccount) 
- console.log('Inserted document: '+result.insertedId)  
-*/
-
-
-
 
 //insertmany method()
  sampleAccounts = [{
@@ -88,9 +45,9 @@ const main = async () =>{
 try{
     await connectToDatabase();
 
- let result= await accountCollection.insertMany(sampleAccounts)            //replace                                
+ let result= await accountCollection.insertMany(sampleAccounts)  //replace                                
  console.log('Inserted'+result.insertedCount+' documents')   //replace
-console.log(result)
+console.log(result)  // many
 } catch (err){
     console.error('Error connecting to the database: '+ err);
 } finally{
